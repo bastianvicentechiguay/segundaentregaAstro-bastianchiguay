92133f52b465bc3a4d78af2d558db15a62a86c42
bastianvicentechiguay <bastianvicente.chiguay@alumnos.ulagos.cl>
2025-11-05 11:17:28 -0300

    Reparación

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 
 
 export default defineConfig({
   integrations: [
-    tailwind(),
-
-   
-    icon()
-  ]
+  ],
 });
